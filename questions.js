79fb3042f75a7004f424b85d0816201080e95e18
Cmoney <cascherer42@gmail.com>
2019-07-22 00:26:22 -0500

    add question banks and base js

---
+++ questions.js
@@ -0,0 +1,36 @@
+<form name="survey" action="">
+
+<div id="alignprose">
+
+<div id="playerlevel">
+
+{139.  What's your age?
+, {name="q237", value="1", answer=15 or less years
+, {name="q237", value="2", answer=16 to 17 years
+, {name="q237", value="3", answer=18 to 21 years
+, {name="q237", value="4", answer=22 to 27 years
+, {name="q237", value="5", answer=28 to 35 years
+, {name="q237", value="6", answer=36 to 45 years
+, {name="q237", value="7", answer=46 to 57 years
+, {name="q237", value="8", answer=58 to 71 years
+, {name="q237", value="9", answer=72 to 87 years
+, {name="q237", value="10", answer=88 or more years
+}
+{140.  For my age, I have...:
+, {name="q238", value="1", answer=...limited life experience. I've done significantly less than every one else.
+, {name="q238", value="2", answer=...a moderate amount of life experience. I've done about as much as everyone else.
+, {name="q238", value="3", answer=...a lot of life experience.  I've done significantly more than every one else.
+
+
+{<u>Optional Stuff:</u>
+<i>Please note that all of these are ENABLED by default!</i>
+
+, {name="detailed" checked="checked" type="checkbox"> Detailed Results
+, {name="html" checked="checked" type="checkbox"> HTML Code (for webpages/journals)
+<!--input name="deity" checked="checked" type="checkbox"-->
+<!-- Associate character with a Forgotten Realms deity - Note that this
+will -->
+<!--b-->
+<!--automatically-->
+<!--/b-->
+<!-- be turned on if your character ends up as a Cleric, Paladin, Ranger, Monk, or Druid!<br-->
