1202b653e14db29a60c5a533b9786d1dd40ed5bf
Cmoney <cascherer42@gmail.com>
2019-07-30 19:17:22 -0500

    Almost complete questionaire function

--- src/components/Questionaire/index.js
+++ src/components/Questionaire/index.js
@@ -5,8 +5,17 @@ import Store from "../../store";
 class PictureCard extends Component {
   const
   questionAnswer = (userAnswer, store) => {
+    const { qValue, characterValue, increase } = userAnswer;
+    console.log(qValue, characterValue, increase);
+    console.log(store.get("character")[qValue][characterValue])
+    let newCharacterValue = parseInt(store.get("character")[qValue][characterValue] + increase)
+    console.log(newCharacterValue)
     // Store the Answer into an array to be analyzed later
-    console.log(userAnswer)
+    store
+    .set("character")[qValue][characterValue](parseInt(newCharacterValue))
+
+    // Store the Answer into an array to be analyzed later
+
     store
       .set('currentAnswers')(store.get('currentAnswers')
         .concat(userAnswer));
@@ -20,35 +29,35 @@ class PictureCard extends Component {
   };
 
   analyzeData = (store) => {
-    
+
   }
 
 
-    render() {
+  render() {
 
-      const { store } = this.props
+    const { store } = this.props
 
-      return (
-        <div className="card bg-dark mt-4">
-          <p>
-            {store.get('currentQuestionBank')[store.get('currentQuestionIndex')].question}
-          </p>
-          {store
-            .get('currentQuestionBank')[store.get('currentQuestionIndex')]
-            .answers
-            .map((answer, index) => (
-              <button
-                key={index}
-                id={index}
-                className="btn btn-primary btn-lg btn-block mycss-HMAnswerButton"
-                onClick={() => this.questionAnswer(answer, store)}
-              >
-                {answer.answer}
-              </button>
-            ))}
-        </div>
-      );
-    }
+    return (
+      <div className="card bg-dark mt-4">
+        <p>
+          {store.get('currentQuestionBank')[store.get('currentQuestionIndex')].question}
+        </p>
+        {store
+          .get('currentQuestionBank')[store.get('currentQuestionIndex')]
+          .answers
+          .map((answer, index) => (
+            <button
+              key={index}
+              id={index}
+              className="btn btn-primary btn-lg btn-block mycss-HMAnswerButton"
+              onClick={() => this.questionAnswer(answer, store)}
+            >
+              {answer.answer}
+            </button>
+          ))}
+      </div>
+    );
   }
+}
 
-  export default Store.withStore(PictureCard);
+export default Store.withStore(PictureCard);
--- src/store.js
+++ src/store.js
@@ -8,48 +8,51 @@ const initialState = {
     currentQuestionBank: [],
     currentAnswers: [],
     currentQuestionIndex: 0,
-    classes: {
-        fighter: 0,
-        ranger: 0,
-        paladin: 0,
-        cleric: 0,
-        druid: 0,
-        bard: 0,
-        monk: 0,
-        sorcerer: 0,
-        barbarian: 0,
-        rogue: 0,
-        wizard: 0,
-        warlock: 0
-    },
-    abilityScores: {
-        strength: 0,
-        dexterity: 0,
-        constitution: 0,
-        intelligence: 0,
-        wisdom: 0,
-        charisma: 0
-    },
-    races: {
-        dwarf: 0,
-        elf: 0,
-        halfElf: 0,
-        halfing: 0,
-        human: 0,
-        halfOrc: 0,
-        gnome: 0
-    },
-    alignments: {
-        lg: 0,
-        ln: 0,
-        le: 0,
-        ng: 0,
-        nn: 0,
-        ne: 0,
-        cg: 0,
-        cn: 0,
-        ce: 0,
-        tie: -1
+    character: {
+        classes: {
+            fighter: 0,
+            ranger: 0,
+            paladin: 0,
+            cleric: 0,
+            druid: 0,
+            bard: 0,
+            monk: 0,
+            sorcerer: 0,
+            barbarian: 0,
+            rogue: 0,
+            wizard: 0,
+            warlock: 0
+        },
+        ability: {
+            strength: 0,
+            dexterity: 0,
+            constitution: 0,
+            intelligence: 0,
+            wisdom: 0,
+            charisma: 0
+        },
+        races: {
+            dwarf: 0,
+            elf: 0,
+            halfElf: 0,
+            halfing: 0,
+            human: 0,
+            halfOrc: 0,
+            gnome: 0
+        },
+        alignments: {
+            lg: 0,
+            ln: 0,
+            le: 0,
+            ng: 0,
+            nn: 0,
+            ne: 0,
+            cg: 0,
+            cn: 0,
+            ce: 0,
+            tie: -1
+        },
+        level: 0
     }
 }
 
